4c8dba9064de4cfa1c907cec500e01e94f395fd0
artguil <46545371+artguil@users.noreply.github.com>
2020-03-29 18:24:23 -0400

    fisnished simply backend

--- routes/api/items.js
+++ routes/api/items.js
@@ -13,4 +13,26 @@ router.get('/', (req, res) => {
         .then(item => res.json(item));
 });
 
+//@route POST api/items
+//@desc Create a Item
+//@access public 
+router.post('/', (req, res) => {
+    const newItem = new Item({
+        name: req.body.name
+    });
+
+    newItem.save().then(item => res.json(item));
+});
+
+//@route DELETE api/items/:id
+//@desc Delete a item
+//@access public 
+router.delete('/:id', (req, res) => {
+    Item.findById(req.param.id).then(item => 
+        item.remove().then(() => res.json({ success: true})))
+        .catch(err => res.status(404).json({ success: false}));
+    });
+
+
+
 module.exports  =  router;
